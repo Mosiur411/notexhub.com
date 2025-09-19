e0139a8bef045af5f44fb56d3335eb3e377ece1b
Mosiur411 <mosiurislamwebdesign@gmail.com>
2025-09-20 02:23:06 +0600

    add my site

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,20 +5,30 @@ import Navbar from "./components/sheard/common/navbar/Navbar";
 import Footer from "./components/sheard/common/footer/Footer";
 import SEOProvider from "@/utils/SEOProvider";
 import Chat from "./components/ChatOpt/Chat";
-import Script from "next/script";
+import Head from "next/head";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Script
-        strategy="afterInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        data-ad-client="ca-pub-4626814401175279"
-        async
-        crossOrigin="anonymous"
-      />
+      <Head>
+        <title>Notex – Trusted Software Development & Digital Partner</title>
+        <meta
+          name="description"
+          content="Notex is your trusted partner for custom software, web, mobile apps, and AI-powered digital transformation solutions."
+        />
+        <meta
+          name="keywords"
+          content="Notex, software development, web development, mobile app development, AI solutions, digital transformation, Bangladesh tech company"
+        />
+        <meta name="author" content="Notex" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-4626814401175279"
+        ></meta>
+      </Head>
       <body className={spaceGrotesk.className}>
         <SEOProvider />
         <AOSInit />
